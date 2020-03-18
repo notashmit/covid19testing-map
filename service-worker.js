847925d9b45012fffcfb6d76ff5c2fb11084df4e
Jorge Caballero <jorge.caballero@icloud.com>
2020-03-18 10:50:56 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19testing-map/precache-manifest.299237fb00653d6f7e56abe46840ada8.js"
+  "/covid19testing-map/precache-manifest.6271623771cfefb59671874400a5551d.js"
 );
 
 self.addEventListener('message', (event) => {
