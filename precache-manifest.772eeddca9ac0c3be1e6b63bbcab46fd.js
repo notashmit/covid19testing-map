e3ee4caa0b8b5a90e74353a149abf6cc4a9d84f3
Jorge Caballero <jorge.caballero@icloud.com>
2020-03-18 22:44:01 -0700

    Updates

---
+++ precache-manifest.772eeddca9ac0c3be1e6b63bbcab46fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f8cfa6268e21dce1d36bff88186482",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7809013b7b9c1fc122c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1882b337e375f96e0033",
+    "url": "/static/js/2.a31ccb43.chunk.js"
+  },
+  {
+    "revision": "33eba8059c08d87cac40e1b60ea6c9ec",
+    "url": "/static/js/2.a31ccb43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7809013b7b9c1fc122c",
+    "url": "/static/js/main.902462ed.chunk.js"
+  },
+  {
+    "revision": "f24690c7e5e138bc8612",
+    "url": "/static/js/runtime-main.bbcdd6a4.js"
+  }
+]);
